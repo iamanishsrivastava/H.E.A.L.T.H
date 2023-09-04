475b0a81774dcb27ea0b06c388e321fa684c3c94
Anish Srivastava <anishamsri@gmail.com>
2023-09-04 20:31:44 +0530

    MediCare 0.0.1 - meds only

--- Javascript/SDKConn.js
+++ Javascript/SDKConn.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-analytics.js";
+import { getFirestore, collection ,query, where, getDocs } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-firestore.js";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,18 +15,21 @@ const firebaseConfig = {
   storageBucket: "medicare-f3618.appspot.com",
   messagingSenderId: "464603648074",
   appId: "1:464603648074:web:4c930223092d5360d056b3",
-  measurementId: "G-K4SWEPK6X2"
+//   measurementId: "G-K4SWEPK6X2"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-// init firebase app
-initializeApp(firebaseConfig)
+// // init firebase app
+// initializeApp(firebaseConfig)
 
 // init services
 const db = getFirestore()
+
+// collection ref
+const colRef = collection(db, 'Medicine')
 
 
 // Assume you have a search input field with id "searchInput" and a button with id "searchButton" in your HTML
@@ -33,18 +38,17 @@ const searchInput = document.getElementById('searchInput');
 const searchButton = document.getElementById('searchButton');
 const resultsDiv = document.getElementById('resultsDiv'); // This is where you'll display the search results
 
-// Add an event listener for the search button click
 searchButton.addEventListener('click', () => {
     const searchTerm = searchInput.value.trim().toLowerCase();
 
     // Create a reference to the "Medicine" collection
     const medicineRef = collection(db, 'Medicine');
 
-    // Construct a query to search for the medicine by name
-    const query = query(medicineRef, where('Name', '==', searchTerm));
+    // Construct a query to search for the medicine by name or symptom
+    const medicineQuery = query(medicineRef, where('Name', '==', searchTerm));
 
     // Execute the query
-    getDocs(query)
+    getDocs(medicineQuery)
         .then((querySnapshot) => {
             // Clear previous search results
             resultsDiv.innerHTML = '';
@@ -57,7 +61,11 @@ searchButton.addEventListener('click', () => {
                 querySnapshot.forEach((doc) => {
                     const medicineData = doc.data();
                     const resultItem = document.createElement('div');
-                    resultItem.textContent = `Name: ${medicineData.Name}, Symptoms: ${medicineData.Symptoms}, Usage: ${medicineData.Usage}`;
+                    resultItem.classList.add('highlight-text'); // Add a CSS class
+                    // Capitalize the first letter of the name and symptoms
+                    const capitalizedName = capitalizeFirstLetter(medicineData.Name);
+                    const capitalizedSymptoms = capitalizeFirstLetter(medicineData.Symptoms);
+                    resultItem.innerHTML = `Name: ${capitalizedName}<br>Symptoms: ${capitalizedSymptoms}<br>Usage: ${medicineData.Usage}`;
                     resultsDiv.appendChild(resultItem);
                 });
             }
@@ -66,3 +74,7 @@ searchButton.addEventListener('click', () => {
             console.error('Error searching for medicine:', error);
         });
 });
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
