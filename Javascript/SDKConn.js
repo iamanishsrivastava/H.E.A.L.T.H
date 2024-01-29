c29911069859eef7707b67d516e1e63c1caae26b
Anish Srivastava <anishamsri@gmail.com>
2024-01-30 00:04:39 +0530

    Realtime DB Update

--- Javascript/SDKConn.js
+++ Javascript/SDKConn.js
@@ -1,35 +1,29 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-analytics.js";
-import { getFirestore, collection ,query, where, getDocs } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-firestore.js";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-analytics.js";
+import { getFirestore, collection ,query, where, getDocs } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-firestore.js";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBli_Me6b_KfAlyyWKB2sF2NEJISY9PvII",
-  authDomain: "medicare-f3618.firebaseapp.com",
-  projectId: "medicare-f3618",
-  storageBucket: "medicare-f3618.appspot.com",
-  messagingSenderId: "464603648074",
-  appId: "1:464603648074:web:4c930223092d5360d056b3",
-//   measurementId: "G-K4SWEPK6X2"
-};
+    apiKey: "AIzaSyA37muoW74fX8Az6cMzde-KKMTK1MUKBeQ",
+    authDomain: "medicare-602b5.firebaseapp.com",
+    projectId: "medicare-602b5",
+    storageBucket: "medicare-602b5.appspot.com",
+    messagingSenderId: "260736167415",
+    appId: "1:260736167415:web:dd175d7bcc15cbffba52f2",
+    measurementId: "G-19ZX6LNE4R"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-// // init firebase app
-// initializeApp(firebaseConfig)
-
 // init services
 const db = getFirestore()
 
 // collection ref
-const colRef = collection(db, 'Medicine')
+const colRef = collection(db, 'medicine')
 
 
 // Assume you have a search input field with id "searchInput" and a button with id "searchButton" in your HTML
@@ -42,7 +36,7 @@ searchButton.addEventListener('click', () => {
     const searchTerm = searchInput.value.trim().toLowerCase();
 
     // Create a reference to the "Medicine" collection
-    const medicineRef = collection(db, 'Medicine');
+    const medicineRef = collection(db, 'medicine');
 
     // Construct a query to search for the medicine by name or symptom
     const medicineQuery = query(medicineRef, where('Name', '==', searchTerm));
@@ -65,7 +59,8 @@ searchButton.addEventListener('click', () => {
                     // Capitalize the first letter of the name and symptoms
                     const capitalizedName = capitalizeFirstLetter(medicineData.Name);
                     const capitalizedSymptoms = capitalizeFirstLetter(medicineData.Symptoms);
-                    resultItem.innerHTML = `Name: ${capitalizedName}<br>Symptoms: ${capitalizedSymptoms}<br>Usage: ${medicineData.Usage}`;
+                    const capitalizedUsage = capitalizeFirstLetter(medicineData.Usage);
+                    resultItem.innerHTML = `Name: ${capitalizedName}<br>Symptoms: ${capitalizedSymptoms}<br>Usage: ${capitalizedUsage}`;
                     resultsDiv.appendChild(resultItem);
                 });
             }
