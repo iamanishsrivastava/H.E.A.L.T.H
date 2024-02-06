0822ff3335f506161b185b211dd3bf1576f5f24d
nawseekhiya <araj80317@gmail.com>
2024-02-06 19:49:33 +0530

    Add home, settings, profile button with responsive changes

--- medicare-proj/src/App.jsx
+++ medicare-proj/src/App.jsx
@@ -4,29 +4,33 @@ function App() {
 
   return (
     <>
-      <section className="main">
+      <main className="main">
         <aside className="sidebar-left">
-          <i className="bi bi-house house-icon"></i>
+          <i className="bi bi-house-fill house-icon"></i>
         </aside>
         <div className="home">
-          <div className="search-container gap-1">
+          <div className="search-container gap-2">
             <div className="search-info">
               info about<br/>medicines/symptoms?
             </div>
             <div className="search-bar">
               <input type="text" className="search-input" placeholder='type here to search' />
+              <div className="iconContainer">
+                <i class="bi bi-filter filter-icon" aria-hidden="true"></i>
+              </div>
             </div>
           </div>
           <div className="medc-container">
-            <p id="options">
+            <p class="options">
               MEDICARE
             </p>
           </div>
         </div>
         <aside className="sidebar-right">
-          d
+          <i class="bi bi-person-circle user-icon"></i>
+          <i class="bi bi-gear settings-icon"></i>
         </aside>
-      </section>
+      </main>
     </>
   )
 }
