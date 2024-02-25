de7059c7c1bf7c4c7ae2010d7dd89483d73dce08
Anish Srivastava <anishamsri@gmail.com>
2024-02-25 18:37:14 +0530

    trial5

--- server/server.mjs
+++ server/server.mjs
@@ -37,6 +37,9 @@ async function connectToDatabase() {
 
 connectToDatabase();
 
+// Serve static files from the 'dist' directory
+app.use(express.static(path.join(__dirname, 'dist')));
+
 // Serve the index.html file from the root folder
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
