de8df1084f7f4d0326111562e0dc384ceb84de2c
nawseekhiya <araj80317@gmail.com>
2024-03-01 16:12:02 +0530

    fix grid system - wip

--- server/config.mjs
+++ server/config.mjs
@@ -1,5 +1,5 @@
 // config.js
 export const username = "teammedicareweb";
-export const password = "3peace70out32";
+export const password = "3peace70ut32";
 export const clusterUrl = "clustermedicare.01dvrqm.mongodb.net";
 export const databaseName = "MediCare";
