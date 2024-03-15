7dc4cf4f31ab5348ca2ab069bdc62aef8afcb2eb
Anish Srivastava <83644811+iamanishsrivastava@users.noreply.github.com>
2024-03-15 20:56:35 +0530

    Delete server/config.mjs
    Signed-off-by: Anish Srivastava <83644811+iamanishsrivastava@users.noreply.github.com>

--- server/config.mjs
+++
@@ -1,5 +0,0 @@
-// config.js
-export const username = "teammedicareweb";
-export const password = "3peace70ut32";
-export const clusterUrl = "clustermedicare.01dvrqm.mongodb.net";
-export const databaseName = "MediCare";
